f1e9d4e8d1c073f521113b32891ed1379177fb65
amr <abuelhassan93@gmail.com>
2021-10-11 00:31:34 +0100

    pass logger in context

--- handler/handler.go
+++ handler/handler.go
@@ -1,15 +1,20 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 )
 
 func GetEmployees(w http.ResponseWriter, r *http.Request) {
-	_, _ = w.Write([]byte("employees list..."))
+	logger, _ := r.Context().Value("logger").(*log.Logger)
+	logger.Println("GetEmployees")
 	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("employees list..."))
 }
 
 func GetClients(w http.ResponseWriter, r *http.Request) {
-	_, _ = w.Write([]byte("clients list..."))
+	logger, _ := r.Context().Value("logger").(*log.Logger)
+	logger.Println("GetClients")
 	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("clients list..."))
 }
--- middleware/logger.go
+++ middleware/logger.go
@@ -1,14 +1,18 @@
 package middleware
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
 )
 
 func Logger() Middleware {
 	return middlewareFunc(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			log.Println("Logger Middleware")
+			l := log.New(os.Stdout, "rid:123 ", log.Ldate|log.Lmicroseconds|log.LUTC)
+			r = r.WithContext(context.WithValue(r.Context(), "logger", l))
 			next.ServeHTTP(w, r)
 		})
 	})
